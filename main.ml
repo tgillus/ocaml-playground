57a05cb9bc306deb9810677cbb35955b970883fa
Tramaine Gillus <1573935+tgillus@users.noreply.github.com>
2022-11-27 21:21:22 -0500

    feat: solve two exercises

--- main.ml
+++ main.ml
@@ -1,11 +1,20 @@
-let x = 50;;
-x * x;;
+let rec last list =
+  match list with
+    [] -> None
+  | first::[] -> Some(first)
+  | _::rest -> last(rest);;
 
-let x = 50 in x * x;;
+let rec last_two list =
+  match list with
+    [] | [_] -> None
+  | [x;  y] -> Some(x, y)
+  | _::rest -> last_two(rest);;
 
-let a = 1 in
-let b = 2 in
-  a + b;;
-
-let square x = x * x;;
-square 50;;
+last [];;
+last [1];;
+last [1; 2];;
+last [1; 2; 3];;
+last_two [];;
+last_two [1];;
+last_two [1; 2];;
+last_two [1; 2; 3]
