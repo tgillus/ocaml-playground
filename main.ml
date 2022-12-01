823234b58cb71b74abd3f9631dfa1525e486dad1
Tramaine Gillus <1573935+tgillus@users.noreply.github.com>
2022-12-01 00:44:57 -0500

    feat: complete nth and length

--- main.ml
+++ main.ml
@@ -1,20 +1,47 @@
-let rec last list =
-  match list with
-    [] -> None
+let rec last = function
+  | [] -> None
   | [x] -> Some x
   | _::rest -> last rest;;
 
-let rec last_two list =
-  match list with
-    [] | [_] -> None
+last [] = None;;
+last [1] = Some 1;;
+last [1; 2] = Some 2;;
+last [1; 2; 3] = Some 3;;
+
+let rec last_two = function
+  | [] | [_] -> None
   | [x;  y] -> Some (x, y)
   | _::rest -> last_two rest;;
 
-last [];;
-last [1];;
-last [1; 2];;
-last [1; 2; 3];;
-last_two [];;
-last_two [1];;
-last_two [1; 2];;
-last_two [1; 2; 3]
+last_two [] = None;;
+last_two [1] = None;;
+last_two [1; 2] = Some (1, 2);;
+last_two [1; 2; 3] = Some (2, 3);;
+
+let rec at list n =
+  match (list, n) with
+  | ([], _) -> None
+  | (first::_, 0) -> Some first
+  | (_::rest, _) -> at rest (n-1);;
+
+at [] 0 = None;;
+at [1] 0 = Some 1;;
+at [1] 1 = None;;
+at [1; 2] 0 = Some 1;;
+at [1; 2] 1 = Some 2;;
+at [1; 2] 2 = None;;
+at [1; 2; 3] 0 = Some 1;;
+at [1; 2; 3] 1 = Some 2;;
+at [1; 2; 3] 2 = Some 3;;
+at [1; 2; 3] 3 = None;;
+
+let rec length list =
+  let rec helper acc = function
+    | [] -> acc
+    | _::rest -> helper (acc + 1) rest
+  in helper 0 list;;
+
+length [] = 0;;
+length [1] = 1;;
+length [1; 2] = 2;;
+length [1; 2; 3] = 3;;
